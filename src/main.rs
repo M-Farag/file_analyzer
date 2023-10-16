58665d32aa0bc9ccfe1374d1f0d76b55cbfbd38d
M-Farag <mina.farag@icloud.com>
2023-10-17 01:51:26 +0300

    ADD A dummy data file
    UPD main to read and print each line from the file

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,23 @@
 use std::fs::File;
-use std::io::BufReader;
+use std::io::{BufRead, BufReader};
 
-fn main() {
+fn main() -> std::io::Result<()>
+{
 
-    let file_handler = file_handler("data.txt");
+    let file_handler = file_handler("data.txt").expect("File not found");
     let buf_reader = BufReader::new(file_handler);
 
+    for line in buf_reader.lines()
+    {
+        let line = line?;
+        println!("{}",line);
+    }
+
+    Ok(())
 }
 
 
-fn file_handler(file_name:&str) -> Result<File,String>
+fn file_handler(file_name:&str) -> Result<File, Box<dyn std::error::Error>>
 {
     let file_handler = File::open(file_name)?;
     Ok(file_handler)
