b00d721e39baea4256236f8e19e3f9a0b61ed0cd
M-Farag <mina.farag@icloud.com>
2023-10-17 01:17:59 +0300

    ADD File readers

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::fs::File;
+use std::io::BufReader;
+
 fn main() {
-    println!("Hello, world!");
+
+    let file_handler = file_handler("data.txt");
+    let buf_reader = BufReader::new(file_handler);
+
+}
+
+
+fn file_handler(file_name:&str) -> Result<File,String>
+{
+    let file_handler = File::open(file_name)?;
+    Ok(file_handler)
 }
